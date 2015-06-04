63cf1f7029484fdbc58a9411b7a85b8158d20231
Gerald Estadieu <gestadieu@usj.edu.mo>
2015-06-04 19:13:46 +0900

    material ui for list and form

--- www/components/material/material-form.controller.js
+++ www/components/material/material-form.controller.js
@@ -9,5 +9,12 @@
 
   function MaterialFormCtrl($scope, material, $cordovaCamera, $cordovaFile) {
     $scope.material = material;
+    if (!angular.isDefined($scope.material.techPair)) { $scope.material.techPair = {}; }
+    
+    $scope.save = function(e) {
+      e.save().then(function(e) {
+        
+      });
+    }
   }
 })();
