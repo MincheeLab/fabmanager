c2198f7cb9fddf14152c4ac8867a0bad0a897f84
Gerald Estadieu <gestadieu@usj.edu.mo>
2015-06-05 15:37:40 +0900

    add $ionicLoading for equipment and material

--- www/components/equipment/equipments.controller.js
+++ www/components/equipment/equipments.controller.js
@@ -5,13 +5,14 @@
     .module('equipment')
     .controller('EquipmentsCtrl', EquipmentsCtrl);
 
-  EquipmentsCtrl.$inject = ['$scope', 'EquipmentCollection'];
+  EquipmentsCtrl.$inject = ['$scope', 'EquipmentCollection', '$ionicLoading'];
 
-  function EquipmentsCtrl($scope, EquipmentCollection) {
-
+  function EquipmentsCtrl($scope, EquipmentCollection, $ionicLoading) {
+    $ionicLoading.show();
     EquipmentCollection.getList().then(function (docs) {
       $scope.collection = docs;
       $scope.equipments = docs.rows;
+      $ionicLoading.hide();
     });
   }
 })();
--- www/components/material/materials.controller.js
+++ www/components/material/materials.controller.js
@@ -5,16 +5,17 @@
     .module('material')
     .controller('MaterialsCtrl', MaterialsCtrl);
 
-  MaterialsCtrl.$inject = ['$scope', 'MaterialCollection', 'presets'];
+  MaterialsCtrl.$inject = ['$scope', 'MaterialCollection', 'presets', '$ionicLoading'];
 
-  function MaterialsCtrl($scope, MaterialCollection, presets) {
-
+  function MaterialsCtrl($scope, MaterialCollection, presets, $ionicLoading) {
+    $ionicLoading.show();
     $scope.presets = presets;
     $scope.data = {shouldShowDelete: false};
 
     MaterialCollection.getList().then(function (docs) {
       $scope.collection = docs;
       $scope.materials = docs.rows;
+      $ionicLoading.hide();
     });
   }
 })();
