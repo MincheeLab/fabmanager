4db17ae343227568c40131956218942025ad9e39
Gerald Estadieu <gestadieu@usj.edu.mo>
2015-06-07 10:53:03 +0900

    booking list view

--- www/components/booking/bookings.controller.js
+++ www/components/booking/bookings.controller.js
@@ -9,6 +9,15 @@
 
   function BookingsCtrl($scope, bookings) {
     $scope.collection = bookings;
-    console.log(bookings);
+
+    $scope.currentDate = null;
+    $scope.isNewDate = function(item, index) {
+      var newDate = item.from.date.split('T')[0];
+      if (newDate !== $scope.currentDate) {
+        $scope.currentDate = newDate;
+        return true;
+      }
+      return false;
+    };
   }
 })();
